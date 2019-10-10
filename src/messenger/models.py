4a0300a3b00ee8472a09928ecd2712d1a421e1b4
belal-bh <bh.pro.pust@gmail.com>
2019-10-10 13:52:23 +0600

    fixed reverse accessor field problem

--- src/messenger/models.py
+++ src/messenger/models.py
@@ -1,3 +1,4 @@
+# messenger.models.py
 from django.db import models
 from django.conf import settings
 
@@ -22,9 +23,11 @@ class Message(models.Model):
         blank=True
     )
     sender = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+        related_name='messenger_user_sender')
     receiver = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+        related_name='messenger_user_receiver')
     parent = models.ForeignKey(
         "self", null=True, blank=True, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
